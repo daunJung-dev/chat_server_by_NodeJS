3a02406f21daa3023ca247934eaf00ed32df7138
dkun9039 <34959472+dkun9039@users.noreply.github.com>
2019-02-21 22:32:46 +0900

    Add files via upload

---
+++ ex08_chat_server.js
@@ -0,0 +1,40 @@
+// ex08_chat_server.js
+
+var http = require('http');
+var express = require('express');
+var app = express();
+var cors = require('cors');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(cors());
+
+var messages = []; //메세지 객체 저장용
+
+app.get('/recieve', function(req, res) {
+    var oldSize = req.query.size;
+    var newSize = messages.length;
+    if(oldSize >= newSize) {
+        res.end();
+    } else {
+        var data = {
+            total: newSize,
+            messages: messages.slice(oldSize)
+        }
+        res.send(JSON.stringify(data));
+    }
+    
+});
+
+app.get('/send', function(req, res) {
+    messages.push({
+        sender:req.query.sender,
+        message:req.query.message
+    });
+    res.end();
+});
+
+var server = http.createServer(app);
+server.listen(app.get('port'), ()=>{
+    console.log('localhsot:%d', app.get('port'));
+});
